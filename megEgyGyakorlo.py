76614e30ae6153bc464f5bf87ebb21f953d9bc31
Ani998 <trinyik.aniko@gmail.com>
2023-02-18 13:01:47 +0100

    megEgyGyakorlo.py feltöltése

---
+++ megEgyGyakorlo.py
@@ -0,0 +1,33 @@
+# converting to integer
+'''float_number = 7.456
+integer_number = int(float_number)
+print(type(float_number))
+print(integer_number)
+print(type(integer_number))'''
+
+#convert to string
+'''stringn = '555'
+print(type(stringn))
+float_to_str = int(stringn)
+print(float_to_str)
+print(type(float_to_str))'''
+
+#convert string to float
+'''string2 = '555'
+string_to_float = float(string2)
+print(f'Típus: {type(string_to_float)}')
+print(string_to_float)'''
+
+# int és float to str
+'''a = 80
+b = 67.678
+
+print(type(a))
+print(type(b))
+print(type(str(a)))
+print(type(str(b)))
+print(str(a))
+print(str(b))'''
+
+
+
