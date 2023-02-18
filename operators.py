667d53a4a41420e098ec63647c66e7857665903a
Ani998 <trinyik.aniko@gmail.com>
2023-02-18 17:03:31 +0100

    operators1

---
+++ operators.py
@@ -0,0 +1,26 @@
+x = 1
+y = 2
+z = 3.2
+
+'''result = x+y+z
+print(result)'''
+
+'''a = 'alma '
+print(a*3)'''
+
+'''print('Osztás: ', 14/3)
+print('Osztás kerekítve:',  14//3)'''
+
+'''print(14.6//2)
+Output: 7.0'''
+
+#print(4.3578**2.4567)
+
+'''a, b, c = 5, 3.4, "alma"
+print(a)
+print(b)
+print(c)'''
+
+
+
+
