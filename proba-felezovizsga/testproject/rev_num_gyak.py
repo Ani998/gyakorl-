4f8adaad4937b78753834d69c7ca02dd694b872a
Ani998 <trinyik.aniko@gmail.com>
2023-03-03 17:55:07 +0100

    rev_num_gyak.py commit

---
+++ proba-felezovizsga/testproject/rev_num_gyak.py
@@ -0,0 +1,16 @@
+# def between(a, b):
+#   pass # write your code here
+
+# print(between(5, 10))
+# >>> [5,6,7,8,9,10]
+
+# print(between(1,2))
+# >>> [1,2]
+
+nr_a = input('Adj eg')
+def number(nr_a, nr_b):
+    number_list = []
+    for x in range(nr_a, nr_b + 1):
+        number_list.append()
+    return (number_list)
+
