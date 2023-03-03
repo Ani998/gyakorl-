c74bdc66aa66b7b2f433c4ce561e475ba5df6693
Ani998 <trinyik.aniko@gmail.com>
2023-03-03 17:38:48 +0100

    special_gyak.py commit

---
+++ proba-felezovizsga/testproject/special_gyak.py
@@ -0,0 +1,17 @@
+def special_eleven(x):
+    if x % 11 <= 1:
+        print('Igaz')
+    else:
+        print('Hamis')
+
+special_eleven(23)
+special_eleven(24)
+special_eleven(122)
+special_eleven(96)
+
+#special_eleven(22)
+# >>> True
+#special_eleven(23)
+# >>> True
+#special_eleven(24)
+# >>> False
