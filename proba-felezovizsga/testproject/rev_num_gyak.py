a456ad253d1ff5f4818116baeeb9b03042ca663e
Ani998 <trinyik.aniko@gmail.com>
2023-03-04 14:44:44 +0100

    rev_num_gyak.py commit

--- proba-felezovizsga/testproject/rev_num_gyak.py
+++ proba-felezovizsga/testproject/rev_num_gyak.py
@@ -7,10 +7,23 @@
 # print(between(1,2))
 # >>> [1,2]
 
-nr_a = input('Adj eg')
-def number(nr_a, nr_b):
-    number_list = []
-    for x in range(nr_a, nr_b + 1):
-        number_list.append()
-    return (number_list)
+def num_list(x, y):
+    return list(range(x, y+1))
+input_x = int(input('Első szám: '))
+input_y = int(input('Második szám: '))
+print(num_list(input_x, input_y))
 
+
+
+
+
+
+
+
+'''def rev_num2(x_nr, y_nr):
+    return list(range(x_nr, y_nr + 1))
+
+
+user_input_x = int(input('Give an integer number : '))
+user_input_y = int(input('Give an integer number : '))
+print(rev_num2(user_input_x, user_input_y))'''
